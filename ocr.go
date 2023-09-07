6c71d4e4f54958599c71eaeec9c4f46cf000f972
Venu Yeredla <venugopal@Venugopals-MacBook-Air.local>
2023-09-07 08:25:20 +0530

    Deleted old files

--- ocr.go
+++ ocr.go
@@ -1,16 +1,14 @@
 package main
 
-import (
-	"fmt"
+//"github.com/otiai10/gosseract"
 
-	"github.com/otiai10/gosseract"
-)
-
-func main() {
-	client := gosseract.NewClient()
-	defer client.Close()
-	client.SetImage("C:\\Work\\WorkEng\\ImgToTe1.png")
-	text, _ := client.Text()
-	fmt.Println(text)
-	// Hello, World!
+func main1() {
+	/*
+	   client := gosseract.NewClient()
+	   defer client.Close()
+	   client.SetImage("C:\\Work\\WorkEng\\ImgToTe1.png")
+	   text, _ := client.Text()
+	   fmt.Println(text)
+	   // Hello, World!
+	*/
 }
